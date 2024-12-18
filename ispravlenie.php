d0c51c7556d0d7f6ad07027baff673493dc56a92
Chipkq <150676751+Chipkq@users.noreply.github.com>
2024-12-18 15:56:16 +0300

    Update ispravlenie.php

--- ispravlenie.php
+++ ispravlenie.php
@@ -7,54 +7,52 @@ if (isset($_GET['Submit'])) {
     // Ensure the input contains only digits
     if (!ctype_digit($id)) {
         $html = "<pre style='color: red;'>Invalid input. User ID must contain only digits.</pre>";
-        echo $html;
-        exit;
-    }
+    } else {
+        $exists = false;
 
-    $exists = false;
+        if ($_DVWA['SQLI_DB'] === MYSQL) {
+            // Check database
+            $query = "SELECT first_name, last_name FROM users WHERE user_id = ?";
 
-    if ($_DVWA['SQLI_DB'] === MYSQL) {
-        // Check database
-        $query = "SELECT first_name, last_name FROM users WHERE user_id = ?";
+            try {
+                $stmt = $GLOBALS["___mysqli_ston"]->prepare($query);
+                $stmt->bind_param("s", $id);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $exists = $result->num_rows > 0;
+            } catch (Exception $e) {
+                $html = "<pre style='color: red;'>There was an error.</pre>";
+            }
 
-        try {
-            $stmt = $GLOBALS["___mysqli_ston"]->prepare($query);
-            $stmt->bind_param("s", $id);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            $exists = $result->num_rows > 0;
-        } catch (Exception $e) {
-            print "There was an error.";
-            exit;
+            ((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
+        } elseif ($_DVWA['SQLI_DB'] === SQLITE) {
+            global $sqlite_db_connection;
+
+            $query = "SELECT first_name, last_name FROM users WHERE user_id = ?";
+            try {
+                $stmt = $sqlite_db_connection->prepare($query);
+                $stmt->bindValue(1, $id, SQLITE3_TEXT);
+                $results = $stmt->execute();
+                $row = $results->fetchArray();
+                $exists = $row !== false;
+            } catch (Exception $e) {
+                $html = "<pre style='color: red;'>There was an error.</pre>";
+            }
+        } else {
+            $html = "<pre style='color: red;'>Unsupported database type.</pre>";
         }
 
-        ((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
-    } elseif ($_DVWA['SQLI_DB'] === SQLITE) {
-        global $sqlite_db_connection;
+        if ($exists) {
+            // Feedback for end user
+            $html .= '<pre>User ID exists in the database.</pre>';
+        } else {
+            // User wasn't found, so the page wasn't!
+            header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
 
-        $query = "SELECT first_name, last_name FROM users WHERE user_id = ?";
-        try {
-            $stmt = $sqlite_db_connection->prepare($query);
-            $stmt->bindValue(1, $id, SQLITE3_TEXT);
-            $results = $stmt->execute();
-            $row = $results->fetchArray();
-            $exists = $row !== false;
-        } catch (Exception $e) {
-            $exists = false;
+            // Feedback for end user
+            $html .= '<pre>User ID is MISSING from the database.</pre>';
         }
-    } else {
-        print "Unsupported database type.";
-        exit;
     }
 
-    if ($exists) {
-        // Feedback for end user
-        $html .= '<pre>User ID exists in the database.</pre>';
-    } else {
-        // User wasn't found, so the page wasn't!
-        header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-
-        // Feedback for end user
-        $html .= '<pre>User ID is MISSING from the database.</pre>';
-    }
+    echo $html;
 }
